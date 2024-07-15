39bff853807755047372ce3a3fa47a48cf46af67
PatelPratik39 <ptl.pratik717@gmail.com>
2024-07-15 11:33:00 -0500

    Majority of the elements using HashMap third trial

---
+++ FindMajorityOfNums.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class FindMajorityOfNums {
+
+//    create a function to find the frequency
+
+    public static void majorityElements(int nums[]){
+//        first step to create a Hash Map
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;  //find the length of an array
+
+//        iterate over the array
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i]) + 1);
+
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        for( int key : map.keySet()){
+            if(map.get(key) > n/3){
+                System.out.println(key);
+            }
+        }
+    }
+
+    public static void main ( String[] args ) {
+        int nums[] = {1, 2, 3, 4, 5, 1, 2, 3, 1, 1};
+        majorityElements(nums);
+    }
+}
---
+++ FindMajorityOfNums3.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class FindMajorityOfNums3 {
+    public static void main ( String[] args ) {
+//        int nums[] = {1,3,2,5,1,3,1,5,1};
+        int nums[] = {1,3,3,3,3,3,3,5,1,3,1,5,1,1};
+        findMajorityElements(nums);
+    }
+
+    private static void findMajorityElements ( int[] nums ) {
+//        1.    Create HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+//        2.    Iterate over an array to check a key-num inside hashmap present ot not,
+//              if present then check frequency if same key comes in loop then add one frequency
+//              if not exist and create a new one if not exists.
+        for(int i = 0; i< n; i++){
+            if (map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+//        3.    Once hashmap is set with updated frequency, then iterate over the map usig key set
+//              and check if the frequency is > n/3 or not, if more that n/3 then print otherwise not.
+
+        for(int key : map.keySet()){
+            if(map.get(key) > n/3){
+                System.out.println(key);
+            }
+        }
+
+    }
+}
