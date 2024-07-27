c75924f2300ea6b42355821aa9a016fccc2bdb37
PatelPratik39 <ptl.pratik717@gmail.com>
2024-07-26 22:38:30 -0500

    Graph Data structure -> Create a Graph with weight

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -7,10 +7,12 @@ public class Graph {
     static class Edge{
         int src;
         int dest;
+        int wt;
 
-        public Edge(int src, int dest){
+        public Edge(int src, int dest, int w){
             this.src = src;
             this.dest = dest;
+            this.wt = w;
         }
     }
     public static void createGraph(ArrayList<Edge> graph[]){
@@ -20,17 +22,17 @@ public class Graph {
         }
 
 
-        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,2, 2));
 
-        graph[1].add(new Edge(1,2));
-        graph[1].add(new Edge(1,3));
+        graph[1].add(new Edge(1,2,10));
+        graph[1].add(new Edge(1,3,0));
 
-        graph[2].add(new Edge(2,0));
-        graph[2].add(new Edge(2,1));
-        graph[2].add(new Edge(2, 3));
+        graph[2].add(new Edge(2,0, 2 ));
+        graph[2].add(new Edge(2,1,10));
+        graph[2].add(new Edge(2, 3, -1));
 
-        graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,2));
+        graph[3].add(new Edge(3,1,0));
+        graph[3].add(new Edge(3,2, -1));
 
     }
 
@@ -39,10 +41,11 @@ public class Graph {
 
         ArrayList < Edge > graph[] = new ArrayList[V];
         createGraph(graph);
+
 //        print neighbors of 2
-        for (int i = 0; i < graph[1].size(); i++) {
-            Edge e = graph[1].get(i);
-            System.out.print(e.dest + " ");
+        for (int i = 0; i < graph[2].size(); i++) {
+            Edge e = graph[2].get(i);
+            System.out.println(e.dest + " , " +  e.wt);
         }
     }
 }
