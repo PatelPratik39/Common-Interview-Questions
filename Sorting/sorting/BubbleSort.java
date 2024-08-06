13c98e3c4d6dd1a41f48e38f4c87129e49ec8c1b
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-05 19:11:25 -0500

    Bubble Sort

---
+++ Sorting/sorting/BubbleSort.java
@@ -0,0 +1,36 @@
+package Sorting.sorting;
+
+public class BubbleSort {
+    private static void bubbleSort ( int[] arr ) {
+        int n = arr.length;
+        boolean swapped;
+//        traverse through an array
+        for(int i =0; i< n-1; i++){
+            swapped = false;
+            for(int j = 0; j < n-1 - i; j++){
+                if(arr[j] > arr[ j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped) break;
+        }
+
+    }
+    public static void main ( String[] args ) {
+        int arr[] = {64,25,12,2,22,64,22,11,9, 5, 6, 2, 33, 4 };
+        System.out.print("Original array: ");
+        printArray(arr);
+        bubbleSort(arr);
+        System.out.print("Sorted array: ");
+        printArray(arr);
+    }
+    private static void printArray ( int[] arr ) {
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
